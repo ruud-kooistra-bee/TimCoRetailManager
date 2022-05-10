8cc8286615442ead5c459d17fd187c5a06eeaa7d
Ruud Kooistra <ruud.kooistra.bee@outlook.com>
2022-05-10 15:03:02 +0200

    Add the Web API Project
    Adds a blank Web API project and tweaks it.

---
+++ TRMDataManager/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+
+namespace TRMDataManager
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}", // API, if you are using api it will try the api routing. If this failed it will try the mvc routing.
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
